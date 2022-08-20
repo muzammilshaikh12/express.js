8346abcc405177097bf9230e4cd22948b7f9aa32
muzammilshaikh12 <110051885+muzammilshaikh12@users.noreply.github.com>
2022-08-21 01:17:46 +0530

    Task 6

--- form.js
+++ form.js
@@ -11,14 +11,24 @@ const adminRoute = require('./routes/admin.js')
 
 const shopRoute = require('./routes/shop.js')
 
+const contactFormroute = require('./routes/contactform.js')
+
+const successRoute = require('./routes/success.js')
+
+const path = require('path')
+
 app.use(bodyParser.urlencoded())
 
 app.use(adminRoute)
 
 app.use(shopRoute)
 
+app.use(contactFormroute)
+
+app.use(successRoute)
+
 app.use('/',(req,res,next)=>{
-    res.status(404).send('<h1>Page not Found</h1>')
+    res.status(404).sendFile(path.join(__dirname,'views','error404.html'))
 })
 app.listen(4000)
 
